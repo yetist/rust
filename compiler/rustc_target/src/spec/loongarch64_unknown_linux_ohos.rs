a025109442faab86db8de4c44039b5249161bcb1
Wu Xiaotian <wuxiaotian@loongson.cn>
2024-06-03 22:03:31 +0800

    loongarch64 musl/ohos triple

--- compiler/rustc_target/src/spec/loongarch64_unknown_linux_ohos.rs
+++ compiler/rustc_target/src/spec/loongarch64_unknown_linux_ohos.rs
@@ -1,17 +1,15 @@
-use crate::spec::{base, Target, TargetOptions};
+use crate::spec::{Target, TargetOptions};
 
 use super::SanitizerSet;
 
 pub fn target() -> Target {
     // LLVM 15 doesn't support OpenHarmony yet, use a linux target instead.
+    let mut base = super::linux_musl_base::opts();
+    base.force_emulated_tls = true;
+    base.has_thread_local = false;
+    base.env =  "ohos".into();
     Target {
-        let mut base = super::linux_musl_base::opts();
-        base.env = "ohos".into();
-        base.crt_static_default = false;
-        base.max_atomic_width = Some(64);
-
         llvm_target: "loongarch64-unknown-linux-musl".into(),
-        description: None,
         pointer_width: 64,
         data_layout: "e-m:e-p:64:64-i64:64-i128:128-n64-S128".into(),
         arch: "loongarch64".into(),
@@ -19,10 +17,9 @@ pub fn target() -> Target {
             cpu: "generic".into(),
             features: "+f,+d".into(),
             llvm_abiname: "lp64d".into(),
-            force_emulated_tls: true,
-            has_thread_local: false,
+            max_atomic_width: Some(64),
+            crt_static_default: false,
             supported_sanitizers: SanitizerSet::ADDRESS
-                | SanitizerSet::CFI
                 | SanitizerSet::LEAK
                 | SanitizerSet::MEMORY
                 | SanitizerSet::MEMTAG
